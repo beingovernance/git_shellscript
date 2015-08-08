e03a88c0698be258f654ac1589497272d2a8df43
我妻 謙樹 <kenjuwagatsuma@r.recruit.co.jp>
2015-08-08 10:38:11 +0900

    first commit

--- github.sh
+++ github.sh
@@ -7,8 +7,28 @@
 # https://github.com/KENJU/git_shellscript 
 
 # read from command line
-# read -p "Enter your Github username: " username
-read -p "Enter your new repository name: " reponame
+username=`git config github.user`
+if [ "$username" = "" ]; then
+	echo "Could not find username, run 'git config --global github.user <username>'"
+	invalid_credentials=1
+fi
+
+dir_name=`basename $(pwd)`
+read -p "Do you want to use '$dir_name' as a repo name?(y/n)" answer_dirname
+case $answer_dirname in
+  y)
+	reponame=$dir_name
+    ;;
+  n)
+	read -p "Enter your new repository name: " reponame
+	if [ "$reponame" = "" ]; then
+		reponame=$dir_name
+	fi
+    ;;
+  *)
+    ;;
+esac
+
 
 # create repo
 echo "Creating Github repository '$reponame' ..."
@@ -31,9 +51,9 @@ git push -u origin master
 echo " done."
 
 # open in a browser
-read -p "Do you want to open the new repo page in browser?(y/n): " answer
+read -p "Do you want to open the new repo page in browser?(y/n): " answer_browser
 
-case $answer in
+case $answer_browser in
   y)
 	echo "Opening in a browser ..."
 	open https://github.com/$username/$reponame
