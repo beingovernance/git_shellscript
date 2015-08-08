8fc841434a2e17a4f0b394e50dc47fa05d2ed73d
我妻 謙樹 <kenjuwagatsuma@r.recruit.co.jp>
2015-08-08 16:13:21 +0900

    updated listing issues in issue.sh

--- issue.sh
+++ issue.sh
@@ -23,13 +23,22 @@ fi
 repo_name=`basename $(git rev-parse --show-toplevel)`
 
 # list all issue
-curl -i https://api.github.com/repos/$username/$repo_name/issues > issues.txt
-awk '/title/{print $2}' issues.txt > tmp1.txt
-awk '/number/{print $2}' issues.txt > tmp2.txt
-paste -d":" tmp1.txt tmp2.txt
+curl https://api.github.com/repos/$username/$repo_name/issues > issues.txt
 
-curl -i https://api.github.com/repos/$username/$repo_name/issues | grep 'title'| sed s/\"title\"://g
-curl -i https://api.github.com/repos/$username/$repo_name/issues | grep 'number'| sed s/\"number\"://g
+# extract texts
+awk '/number/{print $2}' issues.txt > tmp1.txt
+awk '/title/{result = ""; for(i=2;i<=NF;++i) result = result " " $i; print result}' issues.txt > tmp2.txt
+
+# formats
+echo "========================="
+echo "No.\tTitle"
+echo "-------------------------"
+paste -d"\t" tmp1.txt tmp2.txt | tr -d \",
+echo "========================="
+rm tmp{1,2}.txt
+
+# curl -i https://api.github.com/repos/$username/$repo_name/issues | grep 'title'| sed s/\"title\"://g
+# curl -i https://api.github.com/repos/$username/$repo_name/issues | grep 'number'| sed s/\"number\"://g
 
 ###################
 # create an issue
@@ -42,6 +51,23 @@ case $answer_add in
 	read -p "Issue Title: " title
 	read -p "Issue Body: " body
 	curl -u $username -i -H "Content-Type: application/json" -X POST --data '{"title":"'$title'", "body":"'$body'"}' https://api.github.com/repos/$username/$repo_name/issues
+    ;;
+  n)
+    ;;
+  *)
+    ;;
+esac
+
+###################
+# edit an issue
+###################
+
+# POST
+read -p "Do you want to edit issues? (y/n)" answer_edit
+case $answer_edit in
+  y)
+	read -p "Issue Number: " issue_num
+	curl https://api.github.com/repos/$username/$repo_name/issues/$issue_num | grep 'title'| sed s/\"title\"://g
     ;;
   n)
     ;;
